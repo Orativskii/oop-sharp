37cbd35df035ec6a81bd87a66b2fb36f671873a3
Orativskii <71660024+Orativskii@users.noreply.github.com>
2020-11-21 17:15:26 +0200

    Overcoat and flat

---
+++ Flat/Flat/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Flat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Flat flat = new Flat();
+            var inp = flat.Input();
+            Flat o1 = new Flat { Cost = inp[0], Area = inp[1] };
+            Flat o2 = new Flat { Cost = inp[2], Area = inp[3] };
+            String query;
+            Console.WriteLine("Введите знак: == ; = ; >");
+            query = Convert.ToString(Console.ReadLine());
+            switch (query)
+            {
+                case "==":
+                    if (o1 == o2) Console.WriteLine("Площадь квартир одинаковая");
+                    else Console.WriteLine("Площади квартир разные");
+                    break;
+                case "=":
+                    Flat o3 = o1;
+                    Console.WriteLine("Мы присвоили значения первого обьекта в новый третий обьект");
+                    Console.WriteLine(o3.Cost + " " + o3.Area);
+                    break;
+                case ">":
+                    if (o1 > o2) Console.WriteLine("Первая квартира дороже");
+                    else Console.WriteLine("Вторая квартира дороже");
+                    break;
+            }
+        }
+    }
+    class Flat
+    {
+        public Double[] Input()
+        {
+            Console.WriteLine("Введите цену первой квартиры");
+            Double cost = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите площадь первой квартиры");
+            Double area = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите цену второй квартиры");
+            Double cost1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите площадь второй квартиры");
+            Double area1 = Convert.ToDouble(Console.ReadLine());
+            Double[] arr = new Double[4] { cost, area, cost1, area1};
+            return arr;
+        }
+      
+        public Double Cost { get; set; }
+        public Double Area { get; set; }
+
+        public static bool operator ==(Flat o1, Flat o2)
+        {
+            return o1.Area == o2.Area;
+        }
+        public static bool operator !=(Flat o1, Flat o2)
+        {
+            return o1.Area != o2.Area;
+        }
+        public static Flat operator +(Flat o1)
+        {
+            return new Flat
+            {
+                Cost = o1.Cost,
+                Area = o1.Area
+            };
+        }
+        public static bool operator >(Flat o1, Flat o2)
+        {
+            return o1.Cost > o2.Cost;
+        }
+        public static bool operator <(Flat o1, Flat o2)
+        {
+            return o1.Cost < o2.Cost;
+        }
+    }
+}
---
+++ Overcoat/Overcoat/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace Overcoat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Overcoat overcoat = new Overcoat();
+            var titlein = overcoat.TitleInput();
+            var pricein = overcoat.PriceInput();
+            Overcoat o1 = new Overcoat { Title = titlein[0], Price = pricein[0] };
+            Overcoat o2 = new Overcoat { Title = titlein[1], Price = pricein[1] };
+            String query;
+            Console.WriteLine("Введите знак: == ; = ; >");
+            query = Convert.ToString(Console.ReadLine());
+            switch (query)
+            {
+                case "==":
+                    if(o1 == o2) Console.WriteLine("Вещи  одинаковые");
+                    else Console.WriteLine("Вещи разные");
+                    break;
+                case "=":
+                    Overcoat o3 = o1;
+                    Console.WriteLine("Мы присвоили значение первого обьекта в новый третий обьект");
+                    Console.WriteLine(o3.Title + " " + o3.Price);
+                        break;
+                case ">":
+                    if (o1 > o2) Console.WriteLine("Первая вещь дороже");
+                    else Console.WriteLine("Вторая вещь дороже");
+                    break;
+            }
+        }
+    }
+    class Overcoat
+    {
+        public String[] TitleInput()
+        {
+            Console.WriteLine("Введите название первой вещи");
+            String Title1 = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Введите название второй вещи");
+            String Title2 = Convert.ToString(Console.ReadLine());
+            String[] arr = new String[2] { Title1, Title2 };
+            return arr;
+        }
+        public Double[] PriceInput()
+        {
+            Console.WriteLine("Введите цену первой вещи");
+            Double price1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите цену второй вещи");
+            Double price2 = Convert.ToDouble(Console.ReadLine());
+            Double[] arr = new Double[2] { price1, price2 };
+            return arr;
+        }
+        public String Title { get; set; }
+        public Double Price { get; set; }
+
+        public static bool operator ==(Overcoat o1, Overcoat o2)
+        {
+            return o1.Price == o2.Price;
+        }
+        public static bool operator !=(Overcoat o1, Overcoat o2)
+        {
+            return o1.Price != o2.Price;
+        }
+        public static Overcoat operator +(Overcoat o1)
+        {
+            return new Overcoat
+            {
+                Title = o1.Title,
+                Price = o1.Price
+            };
+        }
+        public static bool operator >(Overcoat o1, Overcoat o2)
+        {
+            return o1.Price > o2.Price;
+        }
+        public static bool operator <(Overcoat o1, Overcoat o2)
+        {
+            return o1.Price < o2.Price;
+        }
+    }
+}
